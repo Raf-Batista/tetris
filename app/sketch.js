35dde7848467c5ddf4e810006228c0f87bb59941
Rafael <RafaelB24@yahoo.com>
2020-03-17 15:41:39 -0700

    Add function to create 2d array board, initialize board with 0s and add const for board and square size

--- app/sketch.js
+++ app/sketch.js
@@ -5,3 +5,37 @@ function setup() {
 function draw() {
   
 }
+
+function drawBoard() {
+  let gameBoard = new Array(22);
+  for (let i = 0; i < gameBoard.length; i++) {
+    gameBoard[i] = new Array(10);
+  }
+
+  return gameBoard;
+}
+
+const BOARD = drawBoard();
+const SQ = 35;
+let x = 0;
+let y = 0;
+
+function setup() {
+  createCanvas(1040, 880);
+  frameRate(1)
+  for (let i = 0; i < BOARD.length; i++) {
+    for (let j = 0; j < BOARD[0].length; j++) {
+      BOARD[i][j] = 0
+      rect(j * SQ, i * SQ, SQ, SQ)
+    }
+  }
+  draw()
+}
+
+
+
+function draw() {
+}
+
+
+
